8b624a185b59d4e81004686918f0dbf89e3c8034
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-01-06 15:13:42 -0700

    Fix App styling

---
+++ frontend/src/app/app.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import styled from "styled-components"
+import { useGlobalContext } from './context/GlobalContext';
+
+
+function App() {
+    return (
+        <div>
+            <header>
+                <h5>Hello World</h5>
+            </header>
+        </div>
+    );
+}
+
+export default App;
