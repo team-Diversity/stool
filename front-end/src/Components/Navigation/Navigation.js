132318472691e00d074b43835347310d1b1c77eb
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-01-06 22:59:09 -0700

    Revert "Kinda finish frontend"

--- front-end/src/Components/Navigation/Navigation.js
+++ front-end/src/Components/Navigation/Navigation.js
@@ -22,58 +22,18 @@ const NavigationStyled = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 100%; // Ensure full width
 
     .nav-buttons {
-    display: flex;
-    align-items: center;
-    gap: 16px;
-    padding: 8px;
-    }
-
-    .nav-title {
-    padding: 8px;
-    }
-
-    .nav-buttons {
-    display: flex;
-    align-items: center;
-    gap: 16px;
-    padding: 8px;
-
-        button {
-            background-color: #EFEFEF; 
-            color: #333; 
-            border: none; 
-            border-radius: 5px;
-            padding: 10px 20px; 
-            font-size: 1em; 
-            cursor: pointer; 
-            transition: background-color 0.3s;
-
-            &:hover {
-                background-color: #CCCCCC; 
-            }
-
-            &:focus {
-                outline: none; 
-            }
-        }
+        display: flex;
+        align-items: center;
+        gap: 16px;
+        padding: 8px;
     }
 
     .nav-title {
         padding: 8px;
-    
-        h1 {
-          font-size: 2em; 
-          font-weight: bold; 
-          color: #FFFFFF; 
-          margin: 0; /* Remove default margins */
-          text-transform: uppercase; 
-          letter-spacing: 2px; 
-        }
+        align-self: center
     }
 `;
-
 
 export default Navigation;
