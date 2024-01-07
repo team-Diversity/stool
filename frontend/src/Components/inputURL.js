43c077def23933dedd736028eadfd3c1db840df5
charles01a1 <aina1@ualberta.ca>
2024-01-07 02:40:25 -0700

    download working

--- frontend/src/Components/inputURL.js
+++ frontend/src/Components/inputURL.js
@@ -1,5 +1,6 @@
 "use client"
-import { useState } from 'react'
+import axios from 'axios';
+import { useState } from 'react';
 
 const InputURL = ({ onLoad }) => {
   const [url, setVideo] = useState('')
@@ -15,12 +16,20 @@ const InputURL = ({ onLoad }) => {
     setVideo('')
   }
 
+
+  // const axios = require("axios")
+  const handleDownload = async () => {
+    const data = await axios.get(
+      `http://localhost:4000/download?url=${url}`
+    );
+      };
+
   return (
     <form className='add-form' onSubmit={onSubmit}>
       <div>
         <label htmlFor="urlInput">Enter Video URL:</label>
         <input type="text" id="urlInput" placeholder="Paste video link here" value={url} onChange ={(e) => setVideo(e.target.value)}/>
-        <input type="submit" value='Load URL' className="btn btn-block"></input>
+        <input type="submit" value='Load URL' className="btn btn-block" onClick={handleDownload} ></input>
       </div>
     </form>
   )
